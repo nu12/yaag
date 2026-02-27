baabfcf362fa9bb3ce63b07ed26286f506b7d2e5
nu12 <34694287+nu12@users.noreply.github.com>
2026-02-27 17:03:25 -0500

    Better new session page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resource :signin, only: [ :new, :create ]
-  resource :session, only: [ :new, :destroy ] do
+  resource :session, only: [ :new, :create, :destroy ] do
     get "/:token/create" => "sessions#create", as: :create
   end
 end
